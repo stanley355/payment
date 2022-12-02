4c5e07c839e1b3bc3c30e7346ea9da453d84eb63
Stanley Winata <winatastanley355@gmail.com>
2022-12-02 09:02:37 +0700

    withdrawal dto

--- src/withdrawal/dto/CreateWithdrawalDto.ts
+++ src/withdrawal/dto/CreateWithdrawalDto.ts
@@ -1,3 +1,9 @@
 export interface CreateWithdrawalDto {
-  
+  balanceID: string;
+  userID: string;
+  bankName: 'BCA' | 'GO_PAY';
+  accountNumber: string;
+  accountOwnerName: string;
+  amount: number;
+  message?: string;
 }
