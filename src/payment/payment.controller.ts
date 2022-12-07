7391eaa5868903bc633c40de352537601f0935bc
Stanley Winata <winatastanley355@gmail.com>
2022-12-07 15:29:38 +0700

    remove all previous payment function

--- src/payment/payment.controller.ts
+++ src/payment/payment.controller.ts
@@ -7,27 +7,4 @@ import { UpdatePaidPaymentDto } from './dto/UpdatePaymentDto';
 export class PaymentController {
   constructor(private paymentService: PaymentService) {}
 
-  @Post()
-  async createPayment(
-    @Body() createPaymentDto: CreatePaymentDto,
-  ): Promise<any> {
-    return await this.paymentService.create(createPaymentDto);
-  }
-
-  @Put()
-  async updatePaidPayment(
-    @Body() updatePaymentDto: UpdatePaidPaymentDto
-  ): Promise<any> {
-    return await this.paymentService.updatePaidPayment(updatePaymentDto);
-  }
-
-  @Get('/channels')
-  async viewPaymentsByChannel(@Query('channelID') channelID: number): Promise<any> {
-    return await this.paymentService.findByChannel(channelID);
-  }
-
-  @Get('/subscribers')
-  async viewPaymentsBySubsribers(@Query('subscriberID') subscriberID: string): Promise<any> {
-    return await this.paymentService.findBySubscriber(subscriberID);
-  }
 }
