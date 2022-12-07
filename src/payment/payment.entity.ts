d9c2e1342abae6a6eba87ce9bf03b9ff9077bd15
Stanley Winata <winatastanley355@gmail.com>
2022-12-07 15:39:58 +0700

    payment migration

--- src/payment/payment.entity.ts
+++ src/payment/payment.entity.ts
@@ -2,19 +2,19 @@ import {
   Entity,
   Column,
   PrimaryGeneratedColumn,
-  ManyToOne,
   CreateDateColumn,
 } from 'typeorm';
-import { Balance } from '../balance/balance.entity';
 
-// TODO: Add created at
 @Entity()
 export class Payment {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @ManyToOne((type) => Balance, (balance) => balance.id)
-  balance: string;
+  @Column('uuid')
+  order_id: string;
+
+  @Column('uuid')
+  balance_id: string;
 
   @CreateDateColumn({
     type: 'timestamp',
@@ -22,36 +22,15 @@ export class Payment {
   })
   created_at: Date;
 
-  @CreateDateColumn({
-    type: 'timestamp',
-    default: () => 'CURRENT_TIMESTAMP(0)',
-  })
-  expired_at: Date;
-
   @Column()
   channel_id: number;
-
-  @Column('uuid')
-  subscriber_id: string;
-
-  @Column()
-  subscription_duration: number;
 
   @Column()
   total_amount: number;
 
   @Column()
-  merchant: string;
+  net_amount: number;
 
   @Column()
-  merchant_order_id: string;
-
-  @Column()
-  merchant_payment_link: string;
-
-  @Column({
-    nullable: true,
-    default: 'PENDING'
-  })
-  status: string;
+  platform_free:number;
 }
